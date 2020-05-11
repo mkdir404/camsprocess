253afc504eecaa1ac10dbedfe26134160dc8c787
ricardo <ricardoalba@Ricardos-MacBook-Pro.local>
2020-05-10 20:49:39 -0500

    fix

--- camtest_00/camprocess.sh
+++ camtest_00/camprocess.sh
@@ -5,7 +5,7 @@ ps axu | grep ffmpeg >& catch_proccess_list
 numcam="cam71122"
 ip="admin:123456@192.168.1.65"
 mediastream = "54.86.75.129"
-procexct="ffmpeg -i rtsp://$ip -threads 1 -c:v libx264 -b:v 350K -s 640x360 -c:v copy -c:a copy -f flv rtmp://54.86.75.129/show/$numcam"
+procexct="ffmpeg -i rtsp://$ip -threads 1 -c:v libx264 -b:v 350K -s 640x360 -c:v copy -c:a copy -f flv rtmp://$mediastream/show/$numcam"
 now=$( date )
 
 if grep -qF "$numcam" catch_proccess_list;
