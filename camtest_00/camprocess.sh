eec8e8caaead1eadc04d5494ccbb9be47f2e783e
ricardo <ricardoalba@Ricardos-MacBook-Pro.local>
2020-05-11 15:19:05 -0500

    fix and add args

--- camtest_00/camprocess.sh
+++ camtest_00/camprocess.sh
@@ -1,10 +1,12 @@
 #Este proceso debe correr cada 3 minutos , asi se asegura que si la cam no levanta se cierra el proceso.
+# orden camnumero , ip cam  , mediastream 
+# Ejmplpo de como se pone en el cron ./camprocess.sh cam711220 admin:123456@192.168.1.65 54.86.75.129
+args=("$@")
+numcam=${args[0]}
+ip=${args[1]}
+mediastream=${args[2]}
 
 ps axu | grep ffmpeg >& catch_proccess_list
-
-numcam="cam71122"
-ip="admin:123456@192.168.1.65"
-mediastream = "54.86.75.129"
 procexct="ffmpeg -i rtsp://$ip -threads 1 -c:v libx264 -b:v 350K -s 640x360 -c:v copy -c:a copy -f flv rtmp://$mediastream/show/$numcam"
 now=$( date )
 
